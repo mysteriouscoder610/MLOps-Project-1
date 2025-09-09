8cb8c0ff01e9dce5b7297ae841f954477bd3f400
mysteriouscoder610 <ayush.22620@knit.ac.in>
2025-09-10 01:22:05 +0530

    Project structure ready, virtual environment is set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ayush Jha",
+    author_email="jha2004ayush@gmail.com",
+    packages=find_packages()
+)
